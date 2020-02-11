878c3d3445be3b03db230d640facef6f5a806a95
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-11 21:36:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
 const returnFirstTwoDrivers =function (drivers){
-  return `${drivers.slice(0,2}`;
-}
+  return drivers.slice(0,2);
+};
 
-const returnLastTwoDrivers(drivers){
-  return `${drivers[-1]},${drivers][-2]}`;
+const returnLastTwoDrivers=function(drivers){
+  return drivers.slice(-2);
 }
 
 selectingDrivers 
